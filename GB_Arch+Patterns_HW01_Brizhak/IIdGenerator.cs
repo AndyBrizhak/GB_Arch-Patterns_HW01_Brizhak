df32ebf99fc3437b13aa31c0391022404295af91
AndyBrizhak <andrewbrizhak@gmail.com>
2019-05-19 10:03:23 +0800

    + interface IIdGenerator

---
+++ GB_Arch+Patterns_HW01_Brizhak/IIdGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_Arch_Patterns_HW01_Brizhak
+{
+    public interface IIdGenerator
+    {
+        long CalculateId();
+    }
+}
