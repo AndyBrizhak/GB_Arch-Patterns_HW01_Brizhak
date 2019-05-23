7e84cae3f669acab7e884659c9f9731ef9a77d7d
AndyBrizhak <andrewbrizhak@gmail.com>
2019-05-23 19:49:40 +0800

    delete unusing Task01

--- GB_Arch+Patterns_HW01_Brizhak/Program.cs
+++ GB_Arch+Patterns_HW01_Brizhak/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 //ДЗ по курсу Архитектуры и шаблоны проектирования на C#
 //Задача 1 Провести рефакторинг кода из раздела «Повторяющаяся логика», применяя внедрение
