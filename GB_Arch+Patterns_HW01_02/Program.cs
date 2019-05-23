57b0fbc7780d848aa9fa1425ab73500d41641c05
AndyBrizhak <andrewbrizhak@gmail.com>
2019-05-23 20:04:31 +0800

    + Task02

---
+++ GB_Arch+Patterns_HW01_02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//ДЗ по курсу Архитектуры и шаблоны проектирования на C# Брижак Андрей
+//2. Реализовать программу из раздела «Повторяющиеся фрагменты кода» с помощью делегата Func.
+
+namespace GB_Arch_Patterns_HW01_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
